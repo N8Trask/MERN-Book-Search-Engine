d93067df9dc71e24dad4eea10f839183008800c3
Nate Trask <trask.nate@gmail.com>
2023-08-02 19:42:03 -0500

    added schemas to server side. added dependencies

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+//const uri = 'mongodb+srv://testdb:testdb@<your-cluster-url>/test?retryWrites=true&w=majority';
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
