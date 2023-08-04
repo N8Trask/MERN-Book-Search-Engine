086a989ef926664ef3eef619b6acd6e846cf6760
Nate Trask <trask.nate@gmail.com>
2023-08-03 20:38:33 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 //const uri = 'mongodb+srv://testdb:testdb@<your-cluster-url>/test?retryWrites=true&w=majority';
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dbNate',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
